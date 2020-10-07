6e05acb663dc3f6db902f5bbe1e71198a70af59a
Varun Sheth <vasheth@adobe.com>
2020-10-07 12:02:13 -0400

    stars game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// STAR MATCH - Starting Template
+
+const StarsDisplay = props => (
+    <>
+        {utils.range(1, props.count).map(starId => 
+            <div key={starId} className="star" /> 
+        )}
+    </>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const PlayNumber = props =>  (
+    <button 
+        className="number" 
+        onClick={() => console.log('Num' + props.number)}
+    >
+        {props.number}
+    </button>
+);
+
+const StarMatch = () => {
+    const [stars, setStars] = useState(utils.random(1,9));
+
+    const candidatesAreWrong = utils.sum(candidateNums) > stars;
+
+    const numberStatus = (number) => {
+        if (!availableNums.includes(number)) {
+          return 'used';
+      }
+      if (candidateNums.includes(number)) {
+          return candidatesAreWrong ? 'wrong': 'candidate';
+      }
+      return 'available';
+    };
+
+    return (
+      <div className="game">
+        <div className="help">
+          Pick 1 or more numbers that sum to the number of stars
+        </div>
+        <div className="body">
+          <div className="left">
+            <StarsDisplay count={stars}/>
+          </div>
+          <div className="right">
+            {utils.range(1,9).map(number => 
+                <PlayNumber 
+                    key={number} 
+                    number={number}
+                    status={numberStatus(number)}
+                />
+            )}
+            
+          </div>
+        </div>
+        <div className="timer">Time Remaining: 10</div>
+      </div>
+    );
+  };
+  
+  // Color Theme
+  const colors = {
+    available: 'lightgray',
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
+  };
+  
+  // Math science
+  const utils = {
+    // Sum an array
+    sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+  
+    // create an array of numbers between min and max (edges included)
+    range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+  
+    // pick a random number between min and max (edges included)
+    random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+  
+    // Given an array of numbers and a max...
+    // Pick a random sum (< max) from the set of all available sums in arr
+    randomSumIn: (arr, max) => {
+      const sets = [[]];
+      const sums = [];
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0, len = sets.length; j < len; j++) {
+          const candidateSet = sets[j].concat(arr[i]);
+          const candidateSum = utils.sum(candidateSet);
+          if (candidateSum <= max) {
+            sets.push(candidateSet);
+            sums.push(candidateSum);
+          }
+        }
+      }
+      return sums[utils.random(0, sums.length - 1)];
+    },
+  };
+  
+  ReactDOM.render(<StarMatch />, document.getElementById('root'));
+  
